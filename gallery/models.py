b45d2fe1ee7df001c3e55f686a90acf904a31ea1
victorsz08 <joaovictorsal777@gmail.com>
2024-02-06 09:54:14 -0300

    Configurações do DB e Migrations de Models

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Photografias(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    subtitle = models.CharField(max_length=150, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    picture = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return f"Photografias [name={self.name}]"
--- gallery/views.py
+++ gallery/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
 
+
 def index(request):
-    return render(request, 'gallery/index.html')
+    datas = {
+        1: {
+            "name": "Nebulosa de Carina",
+            "subtitle": "Fonte: Nasa / James Webb"
+        },
+        2: {
+            "name": "Galaxia NGC 1079",
+            "subtitle": "Fonte: Nasa / Hubble"
+        }
+    }
+    return render(request, 'gallery/index.html', {"cards": datas} )
 
 def image(request):
     return render(request, 'gallery/imagem.html')
